be9c19ed3095e4d46c3e7d8741488b0e1d2d8485
mmirbakaitee <126212953+mmirbakaitee@users.noreply.github.com>
2023-03-15 00:48:10 +0200

    Update vsource.cpp

--- vsource.cpp
+++ vsource.cpp
@@ -1,5 +1,10 @@
 #include "studentlib.h"
-const char CDfv[] = "kursiokai.txt";
+const char CDfv[] =  "kursiokai.txt";
+const char CDPfv[] = "studentai.txt";
+const char CRPfv[] = "studentai.txt";
+const char CRGfv[] = "studentugalutiniai.txt";
+const char CRKfv[] = "kietiakai.txt";
+const char CRVfv[] = "varguoliai.txt";
 
 struct student {
     string vardas = "";
@@ -12,6 +17,8 @@ void addstudent(vector<student>& students, student& stud);
 void addgrade(vector<int>& p);
 double getavg(const vector<int>& p);
 double getmedian(const vector<int>& p);
-void print(const vector<student>& students);
 void scan(vector<student>& info, int amount, student& stud);
-bool comp(const student& first, const student& scnd);
+void print(const vector<student>& students); 
+void printPGtxt(vector<student>& info, int amount, student& stud);
+bool compN(const student& first, const student& scnd);
+//bool compV(const student& first, const student& scnd);
