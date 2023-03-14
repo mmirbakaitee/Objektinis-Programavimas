9190f626993ad87eb396b1078ed21de408732f16
mmirbakaitee <126212953+mmirbakaitee@users.noreply.github.com>
2023-03-15 00:49:22 +0200

    Update vheaderis.h

--- vheaderis.h
+++ vheaderis.h
@@ -10,19 +10,16 @@ void addstudent(vector<student>& students, student& stud) {
     int query;
     cout << "Ar norite kad skaitytume faila, jei taip iveskite 'p'.\nKitu atveju - bet kokia raide. \nJusu pasirinkimas: ";
     cin >> choice;
-    if (choice == 'p' || choice == 'P')
-    {
+    if (choice == 'p' || choice == 'P') {
         int n = 0;
         scan(students, n, stud);
-        sort(students.begin(), students.end(), comp);
-        for (int i = 0; i < students.size(); i++)
-        {
+        sort(students.begin(), students.end(), compN);
+        for (int i = 0; i < students.size(); i++) {
             students[i].avg = getavg(students[i].p);
             students[i].median = getmedian(students[i].p);
         }
     }
-    else
-    {
+    else {
         student info;
         cout << "Iveskite studento varda bei pavarde: ";
         cin >> info.vardas >> info.pavarde;
@@ -44,8 +41,7 @@ void addstudent(vector<student>& students, student& stud) {
         }
         cout << "Kokius duomenis naudojant turetu buti isvedami rezultatai? \n Jei ranka rasykite 1, jei atsistiktinai - 2. \n Jusu pasirinkimas: " << endl;
         cin >> query;
-        while (query != 1 && query != 2 || cin.fail())
-        {
+        while (query != 1 && query != 2 || cin.fail()) {
             cin.clear();
             cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
             cout << "SUKLYDOTE! PASIRINKIMAS TURI BUTI SKAICIUS. BANDYKITE DAR KARTA. ";
@@ -70,6 +66,7 @@ void addstudent(vector<student>& students, student& stud) {
         info.median = getmedian(info.p);
         students.push_back(info);
     }
+    
 }
 void addgrade(vector<int>& p) {
     int r;
@@ -112,9 +109,16 @@ double getmedian(const vector<int>& p) {
         return (double)sortedp[sortedp.size() / 2];
     }
 }
-bool comp(const student& first, const student& scnd) {
+bool compN(const student& first, const student& scnd) {
     return first.vardas < scnd.vardas;
 }
+
+/*
+bool compV(const student& first, const student& scnd) {
+    return first.avg * 0.4 + first.e * 0.6 > scnd.avg * 0.4 + scnd.e * 0.6;
+}
+*/
+
 void print(const vector<student>& students) {
     cout << fixed << setprecision(2);
     for (auto i : students)
@@ -124,7 +128,6 @@ void print(const vector<student>& students) {
 void scan(vector<student>& info, int amount, student& stud) {
     string vardas, pavarde, e, t;
     int grades, k = 0;
-    int new_grade;
     ifstream fd(CDfv);
     try {
         if (fd.is_open()) {
@@ -158,6 +161,135 @@ void scan(vector<student>& info, int amount, student& stud) {
     catch (const exception& e) {
         cerr << "KLAIDA: " << e.what() << endl;
     }
+}
+
+void printPGtxt(vector<student>& info, int amount, student& stud) {
+    auto startkur = std::chrono::high_resolution_clock::now();
+    ofstream fr(CRPfv);
+    auto endkur = std::chrono::high_resolution_clock::now();
+    auto durationkur = std::chrono::duration_cast<std::chrono::microseconds>(endkur - startkur);
+    cout << endl;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> pazymys(1, 10);
+    vector<student> kietiakaii;
+    vector<student> varguoliaii;
+    int gradeam;
+    string ammount;
+    bool valid_input = false;
+    cout << "Kiek pazymiu turetu tureti studentas (1-100):  ";
+    cin >> gradeam;
+    while (cin.fail() || gradeam<=0 || gradeam>100)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Iveskite pazymiu kieki tarp 1-100:  ";
+        cin >> gradeam; 
+    }
+    cout << endl;
+    cout << "Kiek irasu turetu buti faile: ";
+    cin >> ammount;
+    while (ammount != "1000" && ammount != "10000" && ammount != "100000" && ammount != "1000000" && ammount != "10000000")
+    {
+        cout << "Netinkama. Galimi pasirinkimo variantai: 1000 10000 100000 1000000 10000000, pasirinkite is naujo:  ";
+        cin >> ammount;
+    }
+
+    amount = stoi(ammount);
+    fr << left << setw(21) << "Vardas" << setw(21) << "Pavarde";
+    for (int i = 1; i < gradeam + 1; i++) {
+        fr << left << "PAZ" << setw(3) << i << "   ";
+    }
+    fr << setw(3) << "Egz." << endl;
+
+    for (int i = 1; i <= amount; i++)
+    {
+        fr << left << "Vardas" << setw(15) << i << "Pavarde" << setw(15) << i;
+        for (int j = 0; j < gradeam; j++) {
+            fr << left << setw(9) << pazymys(gen);
+        }
+        fr <<left << setw(6) << pazymys(gen) << endl;
+
+    }
+    fr.close();
+
+    auto startnus = std::chrono::high_resolution_clock::now();
+    ifstream fq(CDPfv);
+    string line;
+    getline(fq, line);
+    for (int i = 0; i < amount; i++) {
+        fq >> stud.vardas >> stud.pavarde;
+        for (int j = 0; j < gradeam; j++)
+        {
+            int pazz;
+            fq >> pazz;
+            stud.p.push_back(pazz);
+        }
+        fq >> stud.e;
+        stud.avg = getavg(stud.p);
+        stud.median = getmedian(stud.p);
+        info.push_back(stud);
+        stud.p.clear();
+    }
+    fq.close();
+    auto endnus = std::chrono::high_resolution_clock::now();
+    auto durationnus = std::chrono::duration_cast<std::chrono::microseconds>(endnus - startnus);
+
+    ofstream fg(CRGfv); ofstream fk(CRKfv); ofstream fv(CRVfv);
+    fg << fixed << setprecision(2);
+    fg << fixed << setprecision(2);
+    fg << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << left << "Vidurkis" << setw(10) << right << "Mediana" << endl;
+    fg << setfill('-') << setw(60) << "" << setfill(' ') << endl;
+    fk << fixed << setprecision(2);
+    fk << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << left << "Vidurkis" << setw(10) << right << "Mediana" << endl;
+    fk << setfill('-') << setw(60) << "" << setfill(' ') << endl;
+    fv << fixed << setprecision(2);
+    fv << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << left << "Vidurkis" << setw(10) << right << "Mediana" << endl;
+    fv << setfill('-') << setw(60) << "" << setfill(' ') << endl;
+
+    for (auto i : info) {
+        fg << fixed << setprecision(2);
+        fg << left << setw(20) << i.vardas << setw(20) << i.pavarde << setw(15) << i.avg * 0.4 + i.e * 0.6 << setw(15) << i.median * 0.4 + i.e * 0.6 << endl;
+    }
+
+    //sort(info.begin(), info.end(), compV);
+    auto startrus = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < amount; i++) {
+        if (info[i].avg * 0.4 + info[i].e * 0.6 >= 5.00) {
+            kietiakaii.push_back(info[i]);
+        }
+        else {
+            varguoliaii.push_back(info[i]);
+        }
+    }
+        auto endrus = std::chrono::high_resolution_clock::now();
+        auto durationrus = std::chrono::duration_cast<std::chrono::microseconds>(endrus - startrus);
+
+        auto startkiet = std::chrono::high_resolution_clock::now();
+        for (auto i : kietiakaii) {
+            fk << left << setw(20) << i.vardas << setw(20) << i.pavarde << setw(15) << i.avg * 0.4 + i.e * 0.6 << setw(15) << i.median * 0.4 + i.e * 0.6 << endl;
+        }
+        auto endkiet = std::chrono::high_resolution_clock::now();
+        auto durationkiet = std::chrono::duration_cast<std::chrono::microseconds>(endkiet - startkiet);
+
+        auto startvarg = std::chrono::high_resolution_clock::now();
+        for (auto i : varguoliaii) {
+            fv << left << setw(20) << i.vardas << setw(20) << i.pavarde << setw(15) << i.avg * 0.4 + i.e * 0.6 << setw(15) << i.median * 0.4 + i.e * 0.6 << endl;
+        }
+        auto endvarg = std::chrono::high_resolution_clock::now();
+        auto durationvarg = std::chrono::duration_cast<std::chrono::microseconds>(endvarg - startvarg);  
+
+    cout << endl;
+    cout << left << "FAILO SUKURIMO TRUKME:        " << left << durationkur.count() / 1000000.0 << " seconds. " << endl;
+    cout << left << "FAILO NUSKAITYMO TRUKME:      " << left << durationnus.count() / 1000000.0 << " seconds. " << endl;
+    cout << left << "FAILO RUSIAVIMO TRUKME:       " << left << durationrus.count() / 1000000.0 << " seconds. " << endl;
+    cout << left << "KIETIAKU IRASYMO TRUKME:      " << left << durationkiet.count() / 1000000.0 << " seconds. " << endl;
+    cout << left << "VARGUOLIU IRASYMO TRUKME:     " << left << durationvarg.count() / 1000000.0 << " seconds. " << endl;
+    cout << endl;
+    fv.close(); fk.close(); fg.close();
+    info.clear();
+    kietiakaii.clear();
+    varguoliaii.clear();
 }
 
 #endif
