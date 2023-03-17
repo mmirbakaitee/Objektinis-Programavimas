867e966284897da2ab407bb0afe6d9d5d15b7ac8
mmirbakaitee <126212953+mmirbakaitee@users.noreply.github.com>
2023-03-17 23:04:23 +0200

    Update Vectors.v.04.cpp

--- Vectors.v.04.cpp
+++ Vectors.v.04.cpp
@@ -9,13 +9,89 @@ int main() {
     char q = '0';
     char choiceG, choiceM;
     int n = 0;
-    cout << "Ar norite generoti failus? Jei taip iveskite 'd', Kitu atveju betkokia kita raide" << endl;
+    string ammount;
+    cout << "Ar norite generuoti faila? Jei taip iveskite 'd', Kitu atveju betkokia kita raide" << endl;
     cin >> choiceG;
     while (choiceG == 'd' || choiceG == 'D') {
-        printPGtxt(students, n, stud);
+        cout << "Kiek irasu turetu buti faile (1000 10000 100000 1000000 10000000): ";
+        cin >> ammount;
+        while (ammount != "1000" && ammount != "10000" && ammount != "100000" && ammount != "1000000" && ammount != "10000000")
+        {
+            cout << "Netinkama. Galimi pasirinkimo variantai: 1000 10000 100000 1000000 10000000, pasirinkite is naujo:  ";
+            cin >> ammount;
+        }
+        cout << "Ar sugeneruotas failas egzistuoja direktorijoje?\nATSAKYMAS: ";
+        string Filename;
+        if (ammount == "1000") {
+            Filename = "studentai1000.txt";
+            if (FileExists(Filename))
+            {
+                cout << "Failas yra!" << endl;
+                printtxt(students, n, stud, ammount);
+            }
+            else if (!FileExists(Filename))
+            {
+                cout << "Failo nera!" << endl;
+                printPGtxt(students, n, stud, ammount);
+            }
+        }
+        else if (ammount == "10000") {
+            Filename = "studentai10000.txt";
+            if (FileExists(Filename))
+            {
+                cout << "Failas yra!" << endl;
+                printtxt(students, n, stud, ammount);
+            }
+            else if (!FileExists(Filename))
+            {
+                cout << "Failo nera!" << endl;
+                printPGtxt(students, n, stud, ammount);
+            }
+        }
+        else if (ammount == "100000") {
+            Filename = "studentai100000.txt";
+            if (FileExists(Filename))
+            {
+                cout << "Failas yra!" << endl;
+                printtxt(students, n, stud, ammount);
+            }
+            else if (!FileExists(Filename))
+            {
+                cout << "Failo nera!" << endl;
+                printPGtxt(students, n, stud, ammount);
+            }
+        }
+        else if (ammount == "1000000") {
+            Filename = "studentai1000000.txt";
+            if (FileExists(Filename))
+            {
+                cout << "Failas yra!" << endl;
+                printtxt(students, n, stud, ammount);
+            }
+            else if (!FileExists(Filename))
+            {
+                cout << "Failo nera!" << endl;
+                printPGtxt(students, n, stud, ammount);
+            }
+        }
+        else if (ammount == "10000000") {
+            Filename = "studentai10000000.txt";
+            if (FileExists(Filename))
+            {
+                cout << "Failas yra!" << endl;
+                printtxt(students, n, stud, ammount);
+            }
+            else if (!FileExists(Filename))
+            {
+                cout << "Failo nera!" << endl;
+                printPGtxt(students, n, stud, ammount);
+            }
+        }
+
         cout << "Ar norite eiti prie kito pasirinkimo? Jei ne, iveskite 'd', jei taip - betkoks kitas klavisas.\n Jusu pasirinkimas: ";
-        cin >> choiceG; cout << endl;
+        cin >> choiceG; cout << endl; 
     }
+   
     cout << "Galutinai baigti darba? Jei taip iveskite 'm', Kitu atveju betkokia kita raide" << endl;
     cin >> choiceM;
     if (choiceM == 'm' || choiceM == 'M') {
