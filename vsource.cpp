4ca5e8f2895b6eaf70877b1991e23b1e5f6eb9ab
mmirbakaitee <126212953+mmirbakaitee@users.noreply.github.com>
2023-03-17 23:07:10 +0200

    Update vsource.cpp

--- vsource.cpp
+++ vsource.cpp
@@ -1,10 +1,14 @@
 #include "studentlib.h"
-const char CDfv[] =  "kursiokai.txt";
-const char CDPfv[] = "studentai.txt";
-const char CRPfv[] = "studentai.txt";
-const char CRGfv[] = "studentugalutiniai.txt";
-const char CRKfv[] = "kietiakai.txt";
-const char CRVfv[] = "varguoliai.txt";
+char CDfv[] =  "kursiokai.txt";
+char CRPfv[] = "studentai.txt";
+char CRP1fv[] = "studentai1000.txt";
+char CRP2fv[] = "studentai10000.txt";
+char CRP3fv[] = "studentai100000.txt";
+char CRP4fv[] = "studentai1000000.txt";
+char CRP5fv[] = "studentai10000000.txt";
+char CRGfv[] = "studentugalutiniai.txt";
+char CRKfv[] = "kietiakai.txt";
+char CRVfv[] = "varguoliai.txt";
 
 struct student {
     string vardas = "";
@@ -13,12 +17,15 @@ struct student {
     int e = 0;
     double avg = 0, median = 0;
 };
+
+bool FileExists(const std::string& Filename);
 void addstudent(vector<student>& students, student& stud);
 void addgrade(vector<int>& p);
 double getavg(const vector<int>& p);
 double getmedian(const vector<int>& p);
 void scan(vector<student>& info, int amount, student& stud);
 void print(const vector<student>& students); 
-void printPGtxt(vector<student>& info, int amount, student& stud);
+void printPGtxt(vector<student>& info, int amount, student& stud, string ammount);
+void printtxt(vector<student>& info, int amount, student& stud, string ammount);
 bool compN(const student& first, const student& scnd);
-//bool compV(const student& first, const student& scnd);
+bool compV(const student& first, const student& scnd);
